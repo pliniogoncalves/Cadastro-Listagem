905d6b2e7295ceeaa05971c03fae9ee564c00785
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-10-10 21:07:16 -0300

    formulario finalizado

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+class Produto {
+    constructor(nome, valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const cadastroForm = document.getElementById("cadastroProduto");
     const listaProdutos = document.getElementById("listaProdutos");
     const novoProdutoBtn = document.getElementById("novoProdutoBtn");
 
+    // Cria um array para armazenar os objetos de produtos
+    const produtos = [];
+
     cadastroForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
         const nomeProduto = document.getElementById("nomeProduto").value;
-        const valorProduto = document.getElementById("valorProduto").value;
+        const valorProduto = parseFloat(document.getElementById("valorProduto").value);
 
-        // Adicione o novo produto à lista
-        const newRow = listaProdutos.insertRow(-1);
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        cell1.innerHTML = nomeProduto;
-        cell2.innerHTML = valorProduto;
+        // Cria um novo objeto Produto com os dados do formulário
+        const novoProduto = new Produto(nomeProduto, valorProduto);
 
-        // Limpe o formulário
+        // Adiciona o novo produto ao array de produtos
+        produtos.push(novoProduto);
+
+        // Limpa o formulário
         cadastroForm.reset();
 
-        // Ordena a lista de produtos com base no valor (coluna 1)
-        const rows = Array.from(listaProdutos.rows);
-        rows.sort((a, b) => parseFloat(a.cells[1].textContent) - parseFloat(b.cells[1].textContent));
-        rows.forEach(row => listaProdutos.appendChild(row));
+        // Atualiza a tabela de listagem de produtos
+        atualizarTabela(listaProdutos, produtos);
     });
 
     novoProdutoBtn.addEventListener("click", function () {
@@ -32,4 +39,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Oculta o formulário de cadastro inicialmente
     cadastroForm.style.display = "none";
+
+    // Função para atualizar a tabela de listagem de produtos
+    function atualizarTabela(listaProdutos, produtos) {
+        const tbody = listaProdutos.querySelector("tbody");
+
+        // Limpa a tabela removendo todas as linhas
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+
+        // Preencha a tabela com os produtos
+        produtos.forEach(function (produto) {
+            const newRow = tbody.insertRow();
+            const cell1 = newRow.insertCell(0);
+            const cell2 = newRow.insertCell(1);
+            cell1.innerHTML = produto.nome;
+            cell2.innerHTML = produto.valor;
+        });
+
+        // Ordena a lista de produtos com base no valor (coluna 1)
+        const rows = Array.from(tbody.rows);
+        rows.sort((a, b) => parseFloat(a.cells[1].textContent) - parseFloat(b.cells[1].textContent));
+        rows.forEach(row => tbody.appendChild(row));
+    }
 });
